b871938fb64c5059c36eda23da52d661877c834c
Damien <yithanglee@gmail.com>
2024-11-25 11:47:56 +0800

    chore: update dashboard

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,7 +23,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon,
+    // icon: payload.notification.icon,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default function LibraryManagementSystem() {
     <div className="container mx-auto p-4">
    
       <div>
-        <h1 className="text-2xl font-bold mb-6">Accounting System</h1>
+        <h1 className="text-2xl font-bold mb-6">Overview</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         
         </div>
