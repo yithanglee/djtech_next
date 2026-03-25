9d263b4363820c3dad9f3f43447005c2e794afa2
damien <yithanglee@gmail.com>
2026-03-25 13:48:46 +0800

    remove ref no from user

--- src/app/(authenticated)/invoices/page.tsx
+++ src/app/(authenticated)/invoices/page.tsx
@@ -341,7 +341,7 @@ export default function InvoicesPage() {
                     ]}
                     columns={[
                         { label: 'ID', data: 'id' },
-                        { label: 'Ref No', data: 'ref_no' },
+                        // { label: 'Ref No', data: 'ref_no' },
                         { label: 'Due Date', data: 'due_date' },
                         { label: 'Total', data: 'grand_total' },
                         {
